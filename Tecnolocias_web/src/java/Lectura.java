2af303ae86cb0a0aeac6923a443a5e745894267c
Eric <ericto117@gmail.com>
2018-05-29 11:08:41 -0500

    Dibujos incrustado con imagenes propocionadas

--- Tecnolocias_web/src/java/Lectura.java
+++ Tecnolocias_web/src/java/Lectura.java
@@ -23,24 +23,59 @@ public class Lectura extends HttpServlet {
          response.setContentType("text/html;charset=UTF-8");
        PrintWriter out = response.getWriter();
        out.print("<html>\n" +
-"  <head>\n" +
+"  	<head>\n" +
 "	  	<script type=\"text/javascript\" src=\"https://cdnjs.cloudflare.com/ajax/libs/fabric.js/2.2.4/fabric.min.js\"></script>\n" +
 "	  	<script type=\"text/javascript\" src=\"https://ajax.googleapis.com/ajax/libs/angularjs/1.2.6/angular.min.js\"></script>\n" +
 "	  	<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>\n" +
 "	  	<script src=\"http://code.responsivevoice.org/responsivevoice.js\"></script>\n" +
 "	  	<script src=\"JS/Fun.js\"></script>\n" +
-"  </head>\n" +
-"  <body>\n" +
-"    <canvas id=\"canvas\" width=\"600\" height=\"600\" style=\"border: 1px solid #222\"></canvas>\n" +
-"	<br>\n" +
-"    <button id=\"botontexto\">Añadir texto</button>\n" +
-"	<button id=\"botoneliminar\">Eliminar elemento</button>\n" +
-"    <audio autoplay src=\"\" class=\"A\"></audio>\n" +
-"	<a href=\"MisImagenes\" target=\"_blank\"><input type='button' value='Imagenes de mi biblioteca'/></a>\n" +
-"	<br><br>\n" +
-"	<input type='text' name='nom' id='nom' placeholder='Nombre de la pagina' />\n" +
-"	<button id=\"botonguardar\">Guardar</button>\n" +
-"  </body>\n" +
+"	  	<link rel=\"stylesheet\" type=\"text/css\" href=\"CSS/stilo.css\">\n" +
+"  	</head>                                                                                               \n" +
+"  	<body>\n" +
+"		<div class=\"Container\">\n" +
+"	        <div class=\"Content\">\n" +
+"	            <div class=\"Wrapper\">\n" +
+"	                <div class=\"RightContent\">\n" +
+"	                	<div class=\"image-detail\">\n" +
+"		                    <canvas id=\"canvas\" width=\"600\" height=\"600\" style=\"border: 1px solid #222\"></canvas>\n" +
+"							<br>\n" +
+"						    <button id=\"botontexto\">Añadir texto</button>\n" +
+"							<button id=\"botoneliminar\">Eliminar elemento</button>\n" +
+"						    <audio autoplay src=\"\" class=\"A\"></audio>\n" +
+"							<a href=\"MisImagenes\" target=\"_blank\"><input type='button' value='Imagenes de mi biblioteca'/></a>\n" +
+"							<br><br>\n" +
+"							<input type='text' name='nom' id='nom' placeholder='Nombre de la pagina' />\n" +
+"							<button id=\"botonguardar\">Guardar</button><br>\n" +
+"							<input type='text' name='nom' id='cVV' value=\"\" />\n" +
+"							<button id=\"botonrecuperar\">Recuperar</button>\n" +
+"						</div>\n" +
+"	                </div>\n" +
+"	                <div class=\"LeftContent\" id=\"over\">\n" +
+"	                	<img src=\"https://www.anipedia.net/imagenes/donde-viven-los-gatos.jpg\" height=\"150px\" width=\"250px\"/>\n" +
+"				    	<br>\n" +
+"				    	<img src=\"https://www.anipedia.net/imagenes/donde-viven-los-gatos.jpg\" height=\"150px\" width=\"250px\"/>\n" +
+"				    	<br>\n" +
+"				    	<img src=\"https://www.anipedia.net/imagenes/donde-viven-los-gatos.jpg\" height=\"150px\" width=\"250px\"/>\n" +
+"				    	<br>\n" +
+"				    	<img src=\"https://www.anipedia.net/imagenes/donde-viven-los-gatos.jpg\" height=\"150px\" width=\"250px\"/>\n" +
+"				    	<br>\n" +
+"				    	<img src=\"https://www.anipedia.net/imagenes/donde-viven-los-gatos.jpg\" height=\"150px\" width=\"250px\"/>\n" +
+"				    	<br>\n" +
+"				    	<img src=\"https://www.anipedia.net/imagenes/donde-viven-los-gatos.jpg\" height=\"150px\" width=\"250px\"/>\n" +
+"				    	<br>\n" +
+"				    	<img src=\"https://www.anipedia.net/imagenes/donde-viven-los-gatos.jpg\" height=\"150px\" width=\"250px\"/>\n" +
+"				    	<br>\n" +
+"				    	<img src=\"https://www.anipedia.net/imagenes/donde-viven-los-gatos.jpg\" height=\"150px\" width=\"250px\"/>\n" +
+"				    	<br>\n" +
+"				    	<img src=\"https://www.anipedia.net/imagenes/donde-viven-los-gatos.jpg\" height=\"150px\" width=\"250px\"/>\n" +
+"				    	<br>\n" +
+"				    	<img src=\"https://www.anipedia.net/imagenes/donde-viven-los-gatos.jpg\" height=\"150px\" width=\"250px\"/>\n" +
+"				    	<br>\n" +
+"	                </div>\n" +
+"	            </div>\n" +
+"	        </div>\n" +
+"    	</div>\n" +
+"	</body>\n" +
 "</html>");
     }
 }
